80a49c13bbcb992481a5851e98ddce8cf3e3a95e
andrew-kirkham <andrew.kirkham@marquette.edu>
2014-11-03 18:47:16 -0600

    added new rules

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,19 @@ package main;
 
 public class Main {
 	public static void main(String args[]){
+//
+//		String[] eps = new String[] { "small", "big" };
+//		String[] eo = new String[] { "high", "low" };
+//		String[] ppe = new String[] { "low", "fair", "high", "very_high" };
+//		int i=1;
+//		for (String e : eps){
+//			for (String o : eo){
+//				for (String p : ppe){
+//					System.out.println("RULE " + i++ + ": IF ExpertOpinion IS " + o + " AND EarningsPerShare IS " + e 
+//							+ " AND PricePerEarnings IS " + p + " THEN Recommendation IS ");
+//				}
+//			}
+//		}
 		Fuzzy.loadFIS();
 
 		String quoteInput = StockSymbol.quoteDialog();
@@ -12,7 +25,8 @@ public class Main {
 		Fuzzy.fis.setVariable("PricePerEarnings", s.getPe());
 		
 		Fuzzy.fis.evaluate();
-		//Fuzzy.fis.getVariable("Recommendation").chartDefuzzifier(true);
+		Fuzzy.fis.getVariable("Recommendation").chartDefuzzifier(false);
 		Fuzzy.fis.chart();
+		
 	}
 }
